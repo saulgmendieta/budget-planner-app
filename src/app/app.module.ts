9bfd9211a6ef460cc7a823d0199433b9c51ab6a2
saulgmendieta <saulgmendieta@gmail.com>
2024-05-06 19:17:03 -0500

    Initial Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { MenuComponent } from './utilities/menu/menu.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent,
+    MenuComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
